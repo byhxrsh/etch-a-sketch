03caa0f64e1c2c3ad30c83d10e55ecef04dc09d6
harsh89-ai <220668533+harsh89-ai@users.noreply.github.com>
2026-05-30 21:12:47 +0530

    make 16by16 grid using flexbox

---
+++ script.js
@@ -0,0 +1,8 @@
+const parentContainer = document.querySelector(`#container`);
+
+for (let i = 0; i < 256; i++) {
+  let childDivs = document.createElement(`div`);
+	childDivs.id = `childDivs`;
+	parentContainer.appendChild(childDivs);
+}
+
