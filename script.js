d05cac4c75066d104b8b3c753cc04d815b6d3aeb
harsh89-ai <220668533+harsh89-ai@users.noreply.github.com>
2026-06-07 00:23:48 +0530

    add button and make it functional

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-const parentContainer = document.querySelector(`#container`);
+const parentContainer = document.querySelector('#container');
 
 for (let i = 0; i < 256; i++) {
-  let childDiv = document.createElement(`div`);
-	childDiv.id = `childDiv`;
+  let childDiv = document.createElement('div');
+	childDiv.className = 'childDiv';
 	parentContainer.appendChild(childDiv);
-  
-  childDiv.addEventListener(`mouseover`, () => {
-    childDiv.style.backgroundColor = `black`;
+
+  childDiv.addEventListener('mouseover', () => {
+    childDiv.style.backgroundColor = 'black';
   });
 };
+
+const btn = document.querySelector('#select');
+
+btn.addEventListener('click', (event) => {
+  parentContainer.remove();
+
+  let storeInput = prompt('enter a valid size!', 16);
+  let totalCells = storeInput ** 2;
+  console.log(storeInput);
+  console.log(totalCells);
+
+  if (storeInput <= 100) {
+    const updatedContainer = document.createElement('div');
+    updatedContainer.setAttribute('id', 'addContainer');
+    document.body.appendChild(updatedContainer);
+
+    for (i = 0; i < totalCells; i++) {
+      let gridCell = document.createElement('div');
+	    gridCell.className = 'cell';
+	    updatedContainer.appendChild(gridCell);
+
+      gridCell.addEventListener('mouseover', () => {
+        gridCell.style.backgroundColor = 'black';
+      }); 
+    };
+  } else {
+    alert('invalid size selection.');
+  }
+
+});
